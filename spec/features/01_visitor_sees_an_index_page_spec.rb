f9ec0063e482a8c2b7fb9f770863e784b7aadbe7
zhirschtritt <zhirschtritt@gmail.com>
2017-10-17 12:06:26 -0400

    moves test component to frontend foler

--- spec/features/01_visitor_sees_an_index_page_spec.rb
+++ spec/features/01_visitor_sees_an_index_page_spec.rb
@@ -5,18 +5,4 @@ feature "visitor sees an index page" do
     visit sandwiches_path
     expect(page).to have_content "Is that a sandwich?"
   end
-  # scenario "sees a list of sandwiches and link for new sandwich" do
-  #   ham_and_cheese = Sandwich.create( sandwich_name: 'Ham and Cheese')
-  #   hamburger = Sandwich.create( sandwich_name: 'Hamburger' )
-  #
-  #   visit sandwiches_path
-  #
-  #   expect(page).to have_content "Ham and Cheese"
-  #   expect(page).to have_content "Hamburger"
-  #
-  #   click_link  "Add New Sandwich"
-  #
-  #   expect(page).to have_content "New Sandwich Form"
-  # end
-
 end
