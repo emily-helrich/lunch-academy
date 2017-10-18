11f9ca16aa882224f1b0719ac9faad2158da595d
DeniseWalter <denisewalter76@gmail.com>
2017-10-18 16:02:35 -0400

    Changes 'sandwich' view folder to 'pages' and creates pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "sandwiches#index"
-  resources :sandwiches
+  root to: "pages#index"
+
 
   namespace :api do
     namespace :v1 do
