1803d3f3cbf34f141550ee319df5db651961dd27
DeniseWalter <denisewalter76@gmail.com>
2017-10-19 15:37:12 -0400

    resolving merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#index"
-<<<<<<< HEAD
   get '/:id', to: 'pages#show'
-=======
+
   resources :sandwiches
->>>>>>> c3944655e7cdc052e8e51d6ce9b5fc168f1c7a58
 
   namespace :api do
     namespace :v1 do
         resources :sandwiches, only: [:index, :show]
     end
   end
-
 end
