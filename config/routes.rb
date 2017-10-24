b64fbe7a2ab1b1ced98a42c98ca094c4e2389869
DeniseWalter <denisewalter76@gmail.com>
2017-10-24 11:46:54 -0400

    add rails review form

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   root to: "pages#index"
   get '/:id', to: 'pages#show'
 
-  resources :sandwiches
+  resources :sandwiches do
+    resources :reviews
+  end
 
   namespace :api do
     namespace :v1 do
         resources :sandwiches, only: [:index, :show]
     end
   end
+
 end
